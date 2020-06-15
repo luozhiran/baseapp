6db914565f8230651e4dead5aae9d60ab4475802
luozhiran <820152235@qq.com>
2020-06-15 13:51:58 +0800

    gson解析错误

--- net-lib/src/main/java/com/yk/net_lib/Repo.java
+++ net-lib/src/main/java/com/yk/net_lib/Repo.java
@@ -1,9 +1,11 @@
 package com.yk.net_lib;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Repo<T> {
     public int code=-1000;
     public String msg;
-    public String fail;
+    public boolean fail;
     public boolean success;
     public T data;
 }
